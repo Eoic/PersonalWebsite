305743e523c661cfa42f687abd34a69f0ae2226b
Eoic <karolis.strazdas@protonmail.com>
2023-01-29 05:44:49 +0200

    Add favicon.

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,5 @@ from personal.urls import urlpatterns as personal_urls
 urlpatterns = [
     path('', include(personal_urls)),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
