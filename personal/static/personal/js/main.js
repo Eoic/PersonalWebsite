7952ccbab73d67f312138d80e41c52b7c82488e9
idf_karolisstr <karolis.strazdas@indeform.com>
2023-05-28 16:09:24 +0300

    Refactoring, add more info,

--- personal/static/personal/js/main.js
+++ personal/static/personal/js/main.js
@@ -4,7 +4,7 @@
         const currentTheme = nextTheme === 'dark' ? 'light' : 'dark';
         switchTheme(currentTheme, nextTheme, themeSwitchBtn);
     }
-    
+
     const switchTheme = (currentTheme, nextTheme, themeSwitchBtn) => {
         const body = document.body;
         body.setAttribute('data-theme', nextTheme);
@@ -18,80 +18,66 @@
         const body = document.body;
         const currentTheme = body.dataset.theme;
         const nextTheme = event.target.dataset.nextTheme;
-
-        if (nextTheme) {
-            switchTheme(currentTheme, nextTheme, event.target);
-        }
+        switchTheme(currentTheme, nextTheme, event.target);
     }
 
     const handleTabSwitch = (event) => {
-        const main = document.body.getElementsByTagName('main')[0];
-        const sections = main.getElementsByTagName('section');
+        const main = document.querySelector('main');
+        const sections = main.querySelectorAll('section[data-content]');
 
-        [].forEach.call(event.target.parentElement.children, (element) => {
-            if (element === event.target) {
+        Array.from(event.target.parentElement.children).forEach((element) => {
+            if (element === event.target)
                 element.setAttribute('data-active', '');
-                return;
-            }
-
-            element.removeAttribute('data-active');
+            else element.removeAttribute('data-active');
         });
 
-        [].forEach.call(sections, (element) => {
-            if (element.dataset.content === event.target.dataset.contentTarget) {
+        Array.from(sections).forEach((element) => {
+            if (element.dataset.content === event.target.dataset.contentTarget)
                 element.setAttribute('data-active', '');
-                return;
-            }
-
-            element.removeAttribute('data-active');
+            else element.removeAttribute('data-active');
         });
-    }
+    };
 
     const computeEntryTimespans = () => {
-        const timespans = document.querySelectorAll('div[data-timespan]')
-        
-        timespans.forEach((timespan) => {
-            const durationElement = [].find.call(timespan.children, ((item) => item.getAttribute('data-duration') === ''));
+        const timespans = document.querySelectorAll('div[data-timespan]');
+
+        timespans.forEach(timespan => {
+            const durationElement = timespan.querySelector('[data-duration=""]');
             const dateFrom = new Date(durationElement.getAttribute('data-date-from'));
-            const dateTo = durationElement.getAttribute('data-date-to') || new Date();
+            const dateTo = new Date(durationElement.getAttribute('data-date-to') || new Date().toDateString());
             let months = (dateTo.getFullYear() - dateFrom.getFullYear()) * 12;
             months -= dateFrom.getMonth();
             months += dateTo.getMonth();
-            months = months <= 0 ? 0 : months;
+            months = Math.max(months, 0);
 
             if (months < 1) {
-                durationElement.innerHTML = "(Less than a month)"
+                durationElement.innerHTML = "(Less than a month)";
                 return;
             }
 
+            let durationText = '(';
             const years = Math.floor(months / 12);
+
             months = months % 12;
 
-            durationElement.innerHTML = '(';
-
             if (years > 0) {
-                durationElement.innerHTML += `${years} year${years > 1 ? 's' : ''}`;
+                durationText += `${years} year${years > 1 ? 's' : ''}`;
             }
 
             if (months > 0) {
-                durationElement.innerHTML += `${years > 0 ? ', ' : ''}${months} month${months > 1 ? 's' : ''}`;
+                durationText += `${years > 0 ? ', ' : ''}${months} month${months > 1 ? 's' : ''}`;
             }
 
-            durationElement.innerHTML += ')';
+            durationText += ')';
+            durationElement.innerHTML = durationText;
         });
-    }
+    };
 
     document.addEventListener('DOMContentLoaded', () => {
         const themeSwitchBtn = document.getElementById('theme-switcher');
         const tabsNavigator = document.getElementById('tabs');
-
-        if (themeSwitchBtn) {
-            themeSwitchBtn.addEventListener('click', handleThemeSwitch);
-        }
-
-        if (tabsNavigator) {
-            tabsNavigator.addEventListener('click', handleTabSwitch);
-        }
+        themeSwitchBtn.addEventListener('click', handleThemeSwitch);
+        tabsNavigator.addEventListener('click', handleTabSwitch);
 
         computeEntryTimespans();
         loadPreferredTheme(themeSwitchBtn);
