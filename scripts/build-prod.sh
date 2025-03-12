e5313ce3f2cb5fcff8a63e711b7126ec498b93c7
Eoic <karolis.strazdas@protonmail.com>
2025-03-12 23:14:58 +0200

    Remove no longed needed shell script - build-prod.sh.

--- scripts/build-prod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf build ;
-mkdir -p build/{js,css,images} ;
-cp -t build/ assets/images/  ;
-cp -t build/ assets/sitemap.xml assets/*.html;
-npm run chore:minify-js ;
-npm run chore:minify-css ;
