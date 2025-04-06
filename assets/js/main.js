c8acca28a2eca445e0a56b632116aaa61b99c323
Eoic <karolis.strazdas@protonmail.com>
2025-04-06 03:19:53 +0300

    Update background style.,

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,54 @@
         const element = document.documentElement;
         element.classList.toggle('light') && localStorage.setItem('theme', 'light');
         element.classList.toggle('dark') && localStorage.setItem('theme', 'dark');
+        fillBackground();
+    }
+
+    const fillBackground = () => {
+        const canvas = document.getElementById('background');
+        const ctx = canvas.getContext('2d');
+        const charPool = ['~', '#', '$', '%', '!', '?', '@', '^', '&', '*', '(', ')', '-', '+', '{', '}', '<', '>', '\\', '/', '='];
+        const charPoolSize = charPool.length;
+
+        function resizeCanvas() {
+            const dpr = window.devicePixelRatio;
+            const width = Math.ceil(window.innerWidth * dpr);
+            const height = Math.ceil(window.innerHeight * dpr);
+            canvas.width = width;
+            canvas.height = height;
+            canvas.style.width = `${width / dpr}px`;
+            canvas.style.height = `${height / dpr}px`;
+            const ctx = canvas.getContext('2d');
+            ctx.scale(dpr, dpr);
+            drawGrid();
+        }
+
+        function drawGrid() {
+            const cellSize = 60;
+            const cellHalfSize = cellSize / 2;
+            const columns = Math.ceil(canvas.width / cellSize);
+            const rows = Math.ceil(canvas.height / cellSize);
+            const rootStyles = getComputedStyle(document.documentElement);
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.font = '16px Fira Code';
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.globalAlpha = 0.85;
+            ctx.fillStyle = rootStyles.getPropertyValue('color');
+
+            for (let row = 0; row < rows; row++) {
+                for (let col = 0; col < columns; col++) {
+                    const x = col * cellSize;
+                    const y = row * cellSize;
+                    const nextSymbolIndex = Math.floor(Math.random() * charPoolSize);
+                    ctx.fillText(charPool[nextSymbolIndex], x + cellHalfSize, y + cellHalfSize);
+                }
+            }
+        }
+
+        window.addEventListener('resize', resizeCanvas);
+        resizeCanvas();
     }
 
     const computeEntryTimespans = () => {
@@ -18,7 +66,7 @@
             months = Math.max(months, 0);
 
             if (months < 1) {
-                durationElement.innerHTML = "(Less than a month)";
+                durationElement.innerHTML = '(Less than a month)';
                 return;
             }
 
@@ -44,5 +92,6 @@
         const themeSwitchBtn = document.getElementById('theme-switcher');
         themeSwitchBtn.addEventListener('click', handleThemeSwitch);
         computeEntryTimespans();
+        fillBackground();
     });
 })();
